e5774ecb061262c103104cdbcfac81a39213374c
Danil Yaschenko <danyaschenko@gmail.com>
2018-02-21 21:29:40 +0300

    refactor host code, device code

--- testproject/Source.cpp
+++ testproject/Source.cpp
@@ -11,71 +11,69 @@
 
 using namespace std;
 
-const int windowSize = 8;
-
+const int WS = 8;
 
 int cols, rows; //ONLY OF real image(not INCREASED)
 vector<pair<int, int>> ans;
-vector<pair<int, int>> ans1;
+vector<int> ansSum;
 
 
-
-vector<vector<int>> rotate(vector<vector<int>> a) {
+vector<vector<int>> rotate(vector<vector<int>> a, int window) {
 
 	//top
-	for (int i = 0; i < windowSize/2; i++) {
-		for (int j = 0; j < cols + windowSize*2; j++) {
-			swap(a[i][j], a[windowSize-i-1][j]);
+	for (int i = 0; i < window / 2; i++) {
+		for (int j = 0; j < cols + window * 2; j++) {
+			swap(a[i][j], a[window - i - 1][j]);
 		}
 	}
 	//bottom
-	for (int i = rows + windowSize; i < rows + windowSize + windowSize/2; i++) {
-		for (int j = 0; j < cols + windowSize*2; j++) {
-			swap(a[i][j], a[rows+windowSize*2-(windowSize-((rows + windowSize * 2)-i)) - 1][j]);
+	for (int i = rows + window; i < rows + window + window / 2; i++) {
+		for (int j = 0; j < cols + window * 2; j++) {
+			swap(a[i][j], a[rows + window * 2 - (window - ((rows + window * 2) - i)) - 1][j]);
 		}
 	}
 	//left
-	for (int i = 0; i < rows + windowSize * 2; i++) {
-		for (int j = 0; j < windowSize / 2; j++) {
-			swap(a[i][j], a[i][windowSize - j - 1]);
+	for (int i = 0; i < rows + window * 2; i++) {
+		for (int j = 0; j < window / 2; j++) {
+			swap(a[i][j], a[i][window - j - 1]);
 		}
 	}
 	//right
-	for (int i = 0; i < rows + windowSize * 2; i++) {
-		for (int j = cols + windowSize; j < cols + windowSize + windowSize/2; j++) {
-			swap(a[i][j], a[i][cols + windowSize * 2 - (windowSize - ((cols + windowSize * 2) - j)) - 1]);
+	for (int i = 0; i < rows + window * 2; i++) {
+		for (int j = cols + window; j < cols + window + window / 2; j++) {
+			swap(a[i][j], a[i][cols + window * 2 - (window - ((cols + window * 2) - j)) - 1]);
 		}
 	}
 	return a;
 }
 
-vector<vector<int>> initIncreasedImg(vector<vector<int>> a) {
+vector<vector<int>> initIncreasedImg(vector<vector<int>> a, int window) {
 	//top
-	for (int i = 0; i < windowSize; i++) {
-		for (int j = windowSize - 1; j < cols+windowSize-1; j++) {
-			a[i][j] = a[i + windowSize][j];
+	for (int i = 0; i < window; i++) {
+		for (int j = window; j < cols + window; j++) {
+			a[i][j] = a[i + window][j];
 		}
 	}
 	//bottom
-	for (int i = rows + windowSize; i < rows + windowSize * 2; i++) {
-		for (int j = windowSize; j < cols + windowSize; j++) {
-			a[i][j] = a[i - windowSize][j];
+	for (int i = rows + window; i < rows + window * 2; i++) {
+		for (int j = window; j < cols + window; j++) {
+			a[i][j] = a[i - window][j];
 		}
 	}
 	//left
-	for (int i = 0; i < rows + windowSize * 2; i++) {
-		for (int j = 0; j < windowSize; j++) {
-			a[i][j] = a[i][j + windowSize];
+	for (int i = 0; i < rows + window * 2; i++) {
+		for (int j = 0; j < window; j++) {
+			a[i][j] = a[i][j + window];
 		}
 	}
 	//right
-	for (int i = 0; i < rows + windowSize * 2; i++) {
-		for (int j = cols + windowSize; j < cols + windowSize * 2; j++) {
-			a[i][j] = a[i][j - windowSize];
+	for (int i = 0; i < rows + window * 2; i++) {
+		for (int j = cols + window; j < cols + window * 2; j++) {
+			a[i][j] = a[i][j - window];
 		}
 	}
 	//mirroring top corner
-	a = rotate(a);
+	a = rotate(a, window);
 	return a;
 }
 
@@ -118,36 +116,68 @@ vector<vector<int>>  readFile(const string& name) {
 		}
 	}
 
-	vector<vector<int>> pixels(h + windowSize * 2, vector<int>(w + windowSize * 2));
+	//доводим до кратности 8
+	int curWindow = 8;
+	vector<vector<int>> pixels(h + curWindow * 2, vector<int>(w + curWindow * 2));
 	int cnt = 0;
-	for (row = windowSize; row < h + windowSize; row++) {
-		for (col = windowSize; col < w + windowSize; col++) {
+	for (row = curWindow; row < h + curWindow; row++) {
+		for (col = curWindow; col < w + curWindow; col++) {
 			pixels[row][col] = pixs[cnt++];
 		}
 	}
-	return initIncreasedImg(pixels);
+	vector<vector<int>> tmp = initIncreasedImg(pixels, curWindow);
+	
+	int hMulti = (h % WS == 0) ? 0 : WS - (h % WS);
+	int wMulti = (w % WS == 0) ? 0 : WS - (w % WS);
+	if (h % WS != 0 || w % WS != 0) {
+		printf("Image was increased due to multiplicity to WS=%d\nCurrent h:%d, w:%d\n\n", WS, h + hMulti, w + wMulti);
+	}
+	h += hMulti;
+	w += wMulti;
+	rows = h;
+	cols = w;
+	
+
+	vector<vector<int>> pixels1(h + (curWindow * 2), vector<int>(w + (curWindow * 2)));
+	for (row = curWindow; row < h + curWindow; row++) {
+		for (col = curWindow; col < w + curWindow; col++) {
+			pixels1[row][col] = tmp[row][col];
+		}
+	}
+
+	//доводим до кратности 8
+
+	//int curWindow = 8;
+	//vector<vector<int>> pixels(h + curWindow * 2, vector<int>(w + curWindow * 2));
+	//int cnt = 0;
+	//for (row = curWindow; row < h + curWindow; row++) {
+	//	for (col = curWindow; col < w + curWindow; col++) {
+	//		pixels[row][col] = pixs[cnt++];
+	//	}
+	//}
+	return initIncreasedImg(pixels1, curWindow);
 }
 
 int RAE(int block_i, int block_j, int cur_i, int cur_j, vector<vector<int>> &a, vector<vector<int>> &b) {
 	int sum = 0;
-	for (int i = 0; i < windowSize; i++) {
-		for (int j = 0; j < windowSize; j++) {
-			sum += (a[block_i+i][block_j+j] - b[cur_i+i][cur_j+j]) * (a[block_i+i][block_j+j] - b[cur_i+i][cur_j+j]);
+	for (int i = 0; i < WS; i++) {
+		for (int j = 0; j < WS; j++) {
+			sum += (a[block_i + i][block_j + j] - b[cur_i + i][cur_j + j]) * (a[block_i + i][block_j + j] - b[cur_i + i][cur_j + j]);
 		}
 	}
 	return sum;
 }
 
 void motionComp(vector<vector<int>> &a, vector<vector<int>> &b) {
-	for (int block_i = windowSize; block_i <= rows; block_i += windowSize) {
-		for (int block_j = windowSize; block_j <= cols; block_j += windowSize) {
+	for (int block_i = WS; block_i <= rows; block_i += WS) {
+		for (int block_j = WS; block_j <= cols; block_j += WS) {
 			int min = 1000000000;
 			pair<int, int> offset = make_pair(0, 0);
-//#pragma omp parallel for ordered
-			for (int cur_i = block_i - windowSize; cur_i <= block_i+windowSize; cur_i++) {
-				for (int cur_j = block_j - windowSize; cur_j <= block_j + windowSize; cur_j++) {		
+			//#pragma omp parallel for ordered
+			for (int cur_i = block_i - WS; cur_i <= block_i + WS; cur_i++) {
+				for (int cur_j = block_j - WS; cur_j <= block_j + WS; cur_j++) {
 					int tmp = RAE(block_i, block_j, cur_i, cur_j, a, b);
-//#pragma omp ordered
+					//#pragma omp ordered
 					if (tmp < min) {
 						min = tmp;
 						offset = make_pair(cur_i - block_i, cur_j - block_j);
@@ -155,70 +185,9 @@ void motionComp(vector<vector<int>> &a, vector<vector<int>> &b) {
 				}
 			}
 			ans.push_back(offset);
+			ansSum.push_back(min);
 		}
 	}
-}
-
-struct Image {
-	std::vector<char> pixel;
-	int width, height;
-};
-
-Image LoadImage(int* imgGrayScale) {
-	int width = cols+windowSize*2, height=rows+windowSize*2, maxColor=255;
-	std::vector<char> data(width * height * 3);
-
-	for (int i = 0; i < height*width; i++) {
-		data[i] = imgGrayScale[i];
-		data[i+width*height] = imgGrayScale[i];
-		data[i+width*height] = imgGrayScale[i];
-	}
-
-	const Image img = { data, width, height };
-	return img;
-}
-
-Image RGBtoRGBA(const Image& input)
-{
-	Image result;
-	result.width = input.width;
-	result.height = input.height;
-
-	for (std::size_t i = 0; i < input.pixel.size(); i += 3) {
-		result.pixel.push_back(input.pixel[i + 0]);
-		result.pixel.push_back(input.pixel[i + 1]);
-		result.pixel.push_back(input.pixel[i + 2]);
-		result.pixel.push_back(0);
-	}
-
-	return result;
-}
-
-void SaveImage(const Image& img, const char* path)
-{
-	std::ofstream out(path);
-
-	out << "P2\n";
-	out << img.width << " " << img.height << "\n";
-	out << "255\n";
-	for (int i = 0; i < img.width*img.height; i++) {
-		out << img.pixel[i] - '0' << '\n';
-	}
-}
-
-Image RGBAtoRGB(const Image& input)
-{
-	Image result;
-	result.width = input.width;
-	result.height = input.height;
-
-	for (std::size_t i = 0; i < input.pixel.size(); i += 4) {
-		result.pixel.push_back(input.pixel[i + 0]);
-		result.pixel.push_back(input.pixel[i + 1]);
-		result.pixel.push_back(input.pixel[i + 2]);
-	}
-
-	return result;
 }
 
 void err_check(int err, string err_code) {
@@ -235,17 +204,17 @@ void parallelMotionComp(float* a, float* b) {
 	cl_int fn = clGetPlatformIDs(num_entries, platforms, &num_platforms);
 	printf("\n\nNumber of platforms = %d\n", num_platforms);
 
-	cl_device_type device_type = CL_DEVICE_TYPE_ALL;
+	cl_device_type device_type = CL_DEVICE_TYPE_GPU;
 	cl_device_id devices[10];
 	cl_uint num_devices = 0;
 
 	cl_int n1 = clGetDeviceIDs(platforms[0], device_type, num_entries, devices, &num_devices);
 	printf("Number of devices = %d\n", num_devices);
 
-	char deviceNames[200];
+	char deviceNames[10240];
 	printf("------------------------------------------\n");
 	for (int i = 0; i < num_devices; i++) {
-		cl_int aa = clGetDeviceInfo(devices[i], CL_DEVICE_NAME, 10240, deviceNames, NULL);
+		cl_int aa = clGetDeviceInfo(devices[i], CL_DEVICE_NAME, sizeof(deviceNames), deviceNames, NULL);
 		printf("%s\n", deviceNames);
 	}
 	printf("------------------------------------------\n\n");
@@ -265,7 +234,7 @@ void parallelMotionComp(float* a, float* b) {
 		printf("Command queue status: ERROR\n");
 	}
 
-	
+
 	FILE * pFile;
 	size_t lSize;
 	char * buff;
@@ -298,55 +267,48 @@ void parallelMotionComp(float* a, float* b) {
 	cl_int err = clGetProgramBuildInfo(prog, devices[0], CL_PROGRAM_BUILD_LOG, sizeof(buffer), buffer, NULL);
 	printf("%s\n", buffer);
 
-
-	
-	const int IMG_SIZE = (rows+windowSize*2)*(cols+windowSize*2);
-
 	cl_image_format img_fmt;
 	img_fmt.image_channel_order = CL_R;
 	img_fmt.image_channel_data_type = CL_FLOAT;
 	cl_mem image1, image2, image3;
-	size_t width= (cols + windowSize * 2), height= rows + windowSize * 2;
+	size_t width = (cols + WS * 2), height = rows + WS * 2;
 	image1 = clCreateImage2D(context, CL_MEM_READ_ONLY, &img_fmt, width, height, 0, 0, &err);
 	err_check(err, "image1: clCreateImage2D");
 	image2 = clCreateImage2D(context, CL_MEM_READ_ONLY, &img_fmt, width, height, 0, 0, &err);
 	err_check(err, "image2: clCreateImage2D");
 	image3 = clCreateImage2D(context, CL_MEM_READ_WRITE, &img_fmt, width, height, 0, 0, &err);
 	err_check(err, "image3: clCreateImage2D");
-	
 
-	const char *kernel_name = "motion_comp";
+
+	const char *kernel_name = "motion_comp_global";
 	cl_kernel ker = clCreateKernel(prog, kernel_name, &error);
 
-	cl_mem buff_ansX = clCreateBuffer(context, CL_MEM_READ_WRITE, sizeof(int)*(rows / windowSize) * (cols / windowSize), NULL, &error);
-	cl_mem buff_ansY = clCreateBuffer(context, CL_MEM_READ_WRITE, sizeof(int)*(rows / windowSize) * (cols / windowSize), NULL, &error);
+	cl_mem buff_motionVec = clCreateBuffer(context, CL_MEM_READ_WRITE, sizeof(cl_int2)*(rows / WS) * (cols / WS), NULL, &error);
 
 	err = clSetKernelArg(ker, 0, sizeof(image1), &image1);
 	err_check(err, "Arg 1 : clSetKernelArg");
 	err = clSetKernelArg(ker, 1, sizeof(image2), &image2);
 	err_check(err, "Arg 2 : clSetKernelArg");
+	clSetKernelArg(ker, 2, sizeof(buff_motionVec), &buff_motionVec);
+	clSetKernelArg(ker, 3, sizeof(image3), &image3);
 
-	clSetKernelArg(ker, 2, sizeof(buff_ansX), &buff_ansX);
-	clSetKernelArg(ker, 3, sizeof(buff_ansY), &buff_ansY);
-	clSetKernelArg(ker, 4, sizeof(int), &rows);
-	clSetKernelArg(ker, 5, sizeof(int), &cols);
-	clSetKernelArg(ker, 6, sizeof(image3), &image3);
-
-
+	cl_int2* motionVec = new cl_int2[(rows / WS) * (cols / WS)];
 
 	size_t origin[] = { 0,0,0 }; // Defines the offset in pixels in the image from where to write.
 	size_t region[] = { width, height, 1 }; // Size of object to be transferred
 	err = clEnqueueWriteImage(com_queue, image1, CL_TRUE, origin, region, 0, 0, a, 0, NULL, NULL);
 	err = clEnqueueWriteImage(com_queue, image2, CL_TRUE, origin, region, 0, 0, b, 0, NULL, NULL);
-	
 
-	const size_t global_work_size[] = {cols/8, rows/8};
-	const size_t local_work_size[] = {2 , 2};
+
+	const size_t global_work_size[] = { cols / WS , rows / WS, 1};
+	const size_t local_work_size[] = { WS, WS };
 
 
 	cl_event timer;
-	cl_int err_code = clEnqueueNDRangeKernel(com_queue, ker, 2, NULL, global_work_size, 0, NULL, NULL, &timer);
+	cl_int err_code = clEnqueueNDRangeKernel(com_queue, ker, 2, 0, global_work_size, NULL, NULL, NULL, &timer);
+
 	
+
 	if (err_code == 0) {
 		printf("Kernel execution was successfull\n\n");
 	} else {
@@ -354,54 +316,62 @@ void parallelMotionComp(float* a, float* b) {
 	}
 
 	clFinish(com_queue);
-
-	int* answerX = new int[(rows / windowSize) * (cols / windowSize)];
-	int* answerY = new int[(rows / windowSize) * (cols / windowSize)];
-	for (int i = 0; i < (rows / windowSize) * (cols / windowSize); i++) {
-		answerX[i] = 0;
-		answerY[i] = 0;
-	}
-	
-	clEnqueueReadBuffer(com_queue, buff_ansX, CL_TRUE, 0, sizeof(int)*(rows / windowSize) * (cols / windowSize), answerX, NULL, NULL, NULL);
-	clEnqueueReadBuffer(com_queue, buff_ansY, CL_TRUE, 0, sizeof(int)*(rows / windowSize) * (cols / windowSize), answerY, NULL, NULL, NULL);
+	cl_ulong start, end;
+	clGetEventProfilingInfo(timer, CL_PROFILING_COMMAND_START, sizeof(cl_ulong), &start, NULL);
+	clGetEventProfilingInfo(timer, CL_PROFILING_COMMAND_END, sizeof(cl_ulong), &end, NULL);
+	printf("time: %llu\n", end - start);
 	
 
-	for (int i = 0; i < (rows / windowSize) * (cols / windowSize); i++) {
-		cout << answerX[i] << ',' << answerY[i] << ' ';
+	clEnqueueReadBuffer(com_queue, buff_motionVec, CL_TRUE, 0, sizeof(cl_int2)*(rows / WS) * (cols / WS), motionVec, NULL, NULL, NULL);
+
+	bool check = true;
+	for (int i = 0; i < (rows / WS) * (cols / WS); i++) {
+		//cout << "CL:" << motionVec[i].x << ',' << motionVec[i].y << '\n';
+		//cout << "C+:" << ans[i].first << ',' << ans[i].second << ' ' << ansSum[i] << endl << endl;
+		if (motionVec[i].x != ans[i].first || motionVec[i].y != ans[i].second) {
+			check = false;
+			cout << "i=" << i;
+			break;
+		}
 	}
+	if (check) {
+		cout << "Hurray! Motion vectors are correct!";
+	} else {
+		cout << "error :(";
+	}
+
+	
 
 	vector<float> output(height * width);
 	clEnqueueReadImage(com_queue, image3, CL_TRUE, origin, region, 0, 0, output.data(), NULL, NULL, NULL);
-	
+
 
 	ofstream out("TRY.pgm");
-	out << "P2\n" << width << ' ' << height<< "\n255\n";
+	out << "P2\n" << width << ' ' << height << "\n255\n";
 	for (int i = 0; i < width*height; i++) {
 		out << output[i] << '\n';
 	}
 	out.close();
 
-	clReleaseMemObject(buff_ansX);
-	clReleaseMemObject(buff_ansY);
+	clReleaseMemObject(buff_motionVec);
 	clReleaseProgram(prog);
-
 
 }
 
 int main() {
-	
 
-	vector<vector<int>> file1 = readFile("myframe.pgm");
-	vector<vector<int>> file2 = readFile("myframe1.pgm");
+
+	vector<vector<int>> file1 = readFile("frame0015.pgm");
+	vector<vector<int>> file2 = readFile("frame0016.pgm");
 	cout << endl;
 
 
-	float* a = new float[(rows + windowSize * 2)*(cols + windowSize * 2)*3];
-	float* b = new float[(rows + windowSize * 2)*(cols + windowSize * 2)*3];
+	float* a = new float[(rows + WS * 2)*(cols + WS * 2) * 3];
+	float* b = new float[(rows + WS * 2)*(cols + WS * 2) * 3];
 
 	int cnt1 = 0;
-	for (int i = 0; i < rows + windowSize * 2; i++) {
-		for (int j = 0; j < cols + windowSize * 2; j++) {
+	for (int i = 0; i < rows + WS * 2; i++) {
+		for (int j = 0; j < cols + WS * 2; j++) {
 			a[cnt1] = (float)file1[i][j];
 			b[cnt1] = (float)file2[i][j];
 			cnt1++;
@@ -412,24 +382,22 @@ int main() {
 	motionComp(file1, file2);
 	cout << "openMP compensation took:" << float(clock() - begin_time) / CLOCKS_PER_SEC;
 	cout << "\nans.size:" << ans.size() << endl;
-	for (int i = 0; i < ans.size(); i++) {
-		cout << ans[i].first << ',' << ans[i].second << endl;
-	}
 
 	parallelMotionComp(a, b);
 
+
 	cout << endl;
-	
-	
+
+
 
 	vector<vector<int>> compensated = file1;
 
 	int cnt = 0;
-	for (int x = windowSize; x <= rows; x += windowSize) {
-		for (int y = windowSize; y <= cols; y += windowSize) {
-			for (int i = 0; i < windowSize; i++) {
-				for (int j = 0; j < windowSize; j++) {
-					compensated[x + i + ans[cnt].first][y + j + ans[cnt].second] = file1[x+i][y+j];
+	for (int x = WS; x <= rows; x += WS) {
+		for (int y = WS; y <= cols; y += WS) {
+			for (int i = 0; i < WS; i++) {
+				for (int j = 0; j < WS; j++) {
+					compensated[x + i + ans[cnt].first][y + j + ans[cnt].second] = file1[x + i][y + j];
 				}
 			}
 			cnt++;
@@ -438,8 +406,8 @@ int main() {
 
 	ofstream out("result.pgm");
 	out << "P2\n" << cols << ' ' << rows << "\n255\n";
-	for (int i = windowSize; i < rows + windowSize; i++) {
-		for (int j = windowSize; j < cols + windowSize; j++) {
+	for (int i = WS; i < rows + WS; i++) {
+		for (int j = WS; j < cols + WS; j++) {
 			out << compensated[i][j] << '\n';
 		}
 	}
@@ -447,8 +415,8 @@ int main() {
 
 	out = ofstream("diff.pgm");
 	out << "P2\n" << cols << ' ' << rows << "\n255\n";
-	for (int i = windowSize; i < rows + windowSize; i++) {
-		for (int j = windowSize; j < cols + windowSize; j++) {
+	for (int i = WS; i < rows + WS; i++) {
+		for (int j = WS; j < cols + WS; j++) {
 			out << abs(file2[i][j] - compensated[i][j]) << '\n';
 		}
 	}
--- testproject/clfile.cl
+++ testproject/clfile.cl
@@ -1,42 +1,102 @@
 __constant sampler_t sampler = CLK_NORMALIZED_COORDS_FALSE | CLK_ADDRESS_CLAMP_TO_EDGE | CLK_FILTER_NEAREST;
 
-__kernel void motion_comp(__read_only image2d_t a, __read_only image2d_t b, int __global *ansX, int __global *ansY,
-							int rows, int cols, __write_only image2d_t output) {
-	const int wS = 8;
-	const int block_w = get_global_id(0)*8+8;
-	const int block_h = get_global_id(1)*8+8;
-	
-	int ax, ay;
 
-	for (int cur_w = block_w - wS; cur_w <= block_w + wS; cur_w++) {
-		for (int cur_h = block_h - wS; cur_h <= block_h + wS; cur_h++) {
-			int min = 10000000;
-			int sum = 0;
-			for (int i = 0; i < wS; i++) {
-				for (int j = 0; j < wS; j++) {
-					float4 pixelA = read_imagef(a, sampler, (int2)(block_w+i, block_h+j));
-					float4 pixelB = read_imagef(b, sampler, (int2)(cur_w+i, cur_h+j));
-					sum += (int)((pixelA.x-pixelB.x)*(pixelA.x-pixelB.x));
+
+__kernel void motion_comp_local(__read_only image2d_t a, __read_only image2d_t b, 
+							 int2 __global *motionVec, __write_only image2d_t output) {
+	const int WS = 8;
+	const int X = get_group_id(0);
+	const int Y = get_group_id(1);
+
+	const int block_x = X * WS + WS;
+	const int block_y = Y * WS + WS;
+	
+	const int f  =  get_local_id(0);
+	const int f1 =  get_local_id(1);
+
+	const int width = get_image_width(a);
+
+
+	__local float4 A[WS][WS];
+	__local float4 B[WS*3][WS*3];
+	A[f][f1] = read_imagef(a, sampler, (int2)(block_x+f1, block_y+f));
+	
+	for (int i = -1; i <= 1; i++) {
+		for (int j = -1; j <= 1; j++) {	
+			B[(i+1)*WS+f][(j+1)*WS+f1] = read_imagef(b, sampler, (int2)(block_x+f1 + (j*WS), block_y+f + (i*WS)));
+		}
+	}  
+	barrier(CLK_LOCAL_MEM_FENCE);
+
+	__local int mn;
+	mn = 200000000;
+	for (int cur_i = 0; cur_i <= 16; cur_i++){
+		 for (int cur_j = 0; cur_j <= 16; cur_j++) {
+			__private int sum;
+			sum = 0;
+			for (int j = 0; j < WS; j++) {
+				for (int i = 0; i < WS; i++) {
+					__private float4 pix1 = A[i][j];
+					__private float4 pix2 = B[cur_i+i][cur_j+j];
+					sum += (int)((pix1.x - pix2.x)*(pix1.x - pix2.x));
 				}
 			}
-			if (sum < min) {
-				min = sum;
-				ax = cur_w;
-				ay = cur_h;
-				//save (block_w-cur_w)(block_h-cur_h)
+			barrier(CLK_LOCAL_MEM_FENCE);
+			if (sum < mn) {
+				mn = sum;
+				int idx = Y * ((width-WS*2)/WS) + X;
+				motionVec[idx] = (int2)(cur_i-WS, cur_j-WS);
+			}
+			
+		}
+	}
+}
+
+
+
+
+
+
+
+__kernel void motion_comp_global(__read_only image2d_t a, __read_only image2d_t b, 
+							 int2 __global *motionVec, __write_only image2d_t output) {
+	const int WS = 8;
+	const int X = get_global_id(0);
+	const int Y = get_global_id(1);
+
+	const int block_x = X * WS + WS;
+	const int block_y = Y * WS + WS;
+
+	const int width = get_image_width(a);
+	//x - width
+	//y - height
+	
+	int mn = 2000000000;
+	 for (int cur_y = block_y - WS; cur_y <= block_y + WS; cur_y++) {
+		for (int cur_x = block_x - WS; cur_x <= block_x + WS;  cur_x++){
+			int sum = 0;
+			for (int i = 0; i < WS; i++) {
+				for (int j = 0; j < WS; j++) {
+					float4 p1 = read_imagef(a, sampler, (int2)(block_x+i, block_y+j));
+					float4 p2 = read_imagef(b, sampler, (int2)(cur_x+i, cur_y+j));
+					sum += (int)((p1.x - p2.x)*(p1.x - p2.x));
+				}
+			}
+			if (sum < mn) {
+				mn = sum;
+				int idx = Y * ((width-WS*2)/WS) + X;
+				motionVec[idx] = (int2)(cur_y - block_y, cur_x - block_x);
 			}
 		}
 	}
 
-	int idx = get_global_id(0) * 2 + get_global_id(1);
-	ansX[idx] = get_global_id(0);
-	ansY[idx] = get_global_id(1);
-//		for (int i = 0; i < 8; i++) {
-//			for (int j = 0; j < 8; j++) {
-//				pixelA = read_imagef(a, sampler, (int2)(block_i+i, block_j+j));
-//				pixelB = read_imagef(b, sampler, (int2)(block_i+i, block_j+j));
-//				write_imagef(output, (int2)(block_i+i,block_j+j), pixelA);
-//			}
+
+//	for (int i = 0; i < WS; i++) {
+//		for (int j = 0; j < WS; j++) {
+//			float4 px1 = read_imagef(a, sampler, (int2)(block_x+i, block_y+j));
+//			//float4 px2 = read_imagef(b, sampler, (int2)(block_x+i, block_y+j));
+//			write_imagef(output, (int2)(block_x+i, block_y+j), px1);
 //		}
-		
+//	}
+	
 }
